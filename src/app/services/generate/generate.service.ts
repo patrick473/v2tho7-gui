dc5554c1fb5049647259e968f8171f2da68b1f56
TimvdLooy <timvdlooy@gmail.com>
2018-12-23 14:10:31 +0100

    fake generateRule
    fake generateRule where code can be added later

--- src/app/services/generate/generate.service.ts
+++ src/app/services/generate/generate.service.ts
@@ -45,4 +45,8 @@ export class GenerateService {
       )
     );
   }
+
+  generateRule(id: number): void{
+    console.log(`rule: ${id} must be generated in the target database now, the code is not yet here sadly.`);
+  }
 }
