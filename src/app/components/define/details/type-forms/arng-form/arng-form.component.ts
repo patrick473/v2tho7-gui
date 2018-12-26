eba7e29f00c4afb929565cec9f7a9101079a4f9b
patrick kottman <patkottman@gmail.com>
2018-12-26 21:41:40 +0100

    eod

--- src/app/components/define/details/type-forms/arng-form/arng-form.component.ts
+++ src/app/components/define/details/type-forms/arng-form/arng-form.component.ts
@@ -1,15 +1,54 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { validateRange } from 'src/app/helpers/Validations.helper';
 
 @Component({
   selector: 'app-arng-form',
   templateUrl: './arng-form.component.html',
   styleUrls: ['./arng-form.component.css']
 })
+
+
 export class ArngFormComponent implements OnInit {
+
+  arngForm: FormGroup;
+  submitted: false;
 
   constructor() { }
 
   ngOnInit() {
+    const rangeForm = new FormGroup({
+      rangeStart: new FormControl(0, [Validators.required]),
+      rangeEnd: new FormControl(0, [Validators.required])
+    },
+    {
+      validators: validateRange.bind(this)
+    });
+
+    this.arngForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.maxLength(255)]),
+      description: new FormControl('', [Validators.required, Validators.maxLength(255)]),
+      example: new FormControl('', [Validators.required, Validators.maxLength(255)]),
+      column: new FormControl('', [Validators.required, Validators.maxLength(255)]),
+      table: new FormControl('', [Validators.required, Validators.maxLength(255)]),
+      range: rangeForm,
+      inRange: new FormControl('', Validators.required)
+    });
   }
+
+  get name() {return this.arngForm.get('name'); }
+  get description() {return this.arngForm.get('description'); }
+  get example() {return this.arngForm.get('example'); }
+  get column() {return this.arngForm.get('column'); }
+  get table() {return this.arngForm.get('table'); }
+  get range() {return this.arngForm.get('range'); }
+  get rangeStart() {return this.arngForm.get('range.rangeStart'); }
+  get rangeEnd() {return this.arngForm.get('range.rangeEnd'); }
+
+  onSubmit() {
+    console.log('beep');
+  }
+
+
 
 }
