11a1d5bd31186e3f5f84cab93184a26edaed90ec
patrick kottman <patkottman@gmail.com>
2019-01-27 20:51:57 +0100

    changed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,8 @@ import { TypeDetailsComponent } from './components/define/type/type-details/type
 import { RuleTypeSelectComponent } from './components/define/rule/rule-type-select/rule-type-select.component';
 import { RuleDetailsComponent } from './components/define/rule/rule-details/rule-details.component';
 import {HomeComponent} from 'src/app/components/home/home.component';
+import { TemplateOverviewComponent } from './components/define/template/template-overview/template-overview.component';
+import { TemplateDetailsComponent } from './components/define/template/template-details/template-details.component';
 
 
 
@@ -38,7 +40,8 @@ const routes: Routes = [
   {path: 'define/rule/details', component: RuleDetailsWrapperComponent},
   {path: 'define/type', component: TypeOverviewComponent},
   {path: 'define/type/details', component: TypeDetailsComponent},
-  {path: 'generate', component: GenerateOverviewComponent}
+  {path: 'define/template', component: TemplateOverviewComponent},
+  {path: 'define/template/details', component: TemplateDetailsComponent}
 ];
 
 @NgModule({
