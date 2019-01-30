21199ebfa7def9b44b8759fc6c7399a6a6c3a91f
patrick kottman <patkottman@gmail.com>
2019-01-30 19:43:14 +0100

    finished regular form fields

--- src/app/components/define/rule/rule-details/rule-details.component.ts
+++ src/app/components/define/rule/rule-details/rule-details.component.ts
@@ -16,33 +16,29 @@ import {BusinessRule} from 'src/app/models/BusinessRule';
 export class RuleDetailsComponent implements OnInit {
 
   form: FormGroup;
-
-  types: BusinessRuleType[];
+  ruletype: number;
+  ruletypes: BusinessRuleType[] = [];
 
   bindingLabels = ['a', 'b', 'c'];
-  constructor(private businessruleService: BusinessruleService, private businessRuleTypeService: BusinessRuleTypeService) {
+  constructor(private businessruleService: BusinessruleService, private businessRuleTypeService: BusinessRuleTypeService, private businessruleTypeService: BusinessRuleTypeService) {
 
   }
 
   async ngOnInit() {
     this.form = new FormGroup({
       name: new FormControl('', Validators.required),
-      constraint: new FormControl(false, Validators.required), // Set this value to something else if the detail is an update instead of new create.
+      constraint: new FormControl(false, Validators.required),
       operator: new FormControl('', Validators.required),
-      bindings: new FormArray([]),
       type: new FormControl('', Validators.required),
       table: new FormControl('', Validators.required),
-      modifyTypes: new FormGroup({
-        insert: new FormControl(false, Validators.required), // Set this value to something else if the detail is an update instead of new create.
-        update: new FormControl(false, Validators.required), // Set this value to something else if the detail is an update instead of new create.
-        delete: new FormControl(false, Validators.required) // Set this value to something else if the detail is an update instead of new create.
-      }),
-      error: new FormControl('', Validators.required)
+      insert: new FormControl(false, Validators.required),
+      update: new FormControl(false, Validators.required),
+      delete: new FormControl(false, Validators.required),
+      error: new FormControl('', Validators.required),
+      bindings: new FormArray([]),
     });
-    this.types = await this.businessRuleTypeService.getBusinessRuleTypes();
-    this.bindings.push(new FormControl('a'));
-    this.bindings.push(new FormControl('b'));
-    this.bindings.push(new FormControl('c'));
+    this.getBusinessruleTypes();
+
   }
 
   async onSubmit() {
@@ -70,4 +66,10 @@ export class RuleDetailsComponent implements OnInit {
   get bindings() {
     return this.form.get('bindings') as FormArray;
   }
+  async getBusinessruleTypes() {
+    this.ruletypes = await this.businessRuleTypeService.getBusinessRuleTypes();
+  }
+  onSelectChange() {
+    console.log(this.ruletype);
+}
 }
